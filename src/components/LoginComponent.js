990490eea6a6b0ae7bf9629e8aca7d3c0ca0acf2
zefan-zhang <kd.zefan@gmail.com>
2020-07-29 16:11:05 -0400

    updated login and register page outlook

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default class LoginComponent extends React.Component {
     state = {
@@ -28,40 +29,72 @@ export default class LoginComponent extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <input
-                    placeholder="username"
-                    value={this.state.user.username}
-                    onChange={(e) => {
-                        const username = e.target.value;
-                        this.setState(prevState => ({
-                            user: {
-                                ...prevState.user,
-                                username: username
+            <div className="container mt-5">
+                <h2 className="text-center m-3">Welcome</h2>
+                <div className="form-group row">
+                    <div className="col-sm-2">
+                        <label for="username">
+                        Username
+                        </label>
+                    </div>
+                    <div className="col-sm-10">
+                        <input
+                            className="form-control"
+                            placeholder="username"
+                            id = "username"
+                            value={this.state.user.username}
+                            onChange={(e) => {
+                                const username = e.target.value;
+                                this.setState(prevState => ({
+                                    user: {
+                                        ...prevState.user,
+                                        username: username
+                                    }
+                                }))
                             }
-                        }))
-                    }
-                    }/>
-                <br/>
-                <input
-                    placeholder="password"
-                    type="password"
-                    value={this.state.user.password}
-                    onChange={(e) => {
-                        const password = e.target.value;
-                        this.setState(prevState => ({
-                            user: {
-                                ...prevState.user,
-                                password: password
+                            }/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="col-sm-2">
+                        <label htmlFor="password">
+                            Password
+                        </label>
+                    </div>
+                    <div className="col-sm-10">
+                        <input
+                            className="form-control"
+                            placeholder="password"
+                            type="password"
+                            id = "password"
+                            value={this.state.user.password}
+                            onChange={(e) => {
+                                const password = e.target.value;
+                                this.setState(prevState => ({
+                                    user: {
+                                        ...prevState.user,
+                                        password: password
+                                    }
+                                }))
                             }
-                        }))
-                    }
-                    }/>
-                <br/>
-                <button onClick={this.login}>
-                    Login
-                </button>
+                            }/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <span className="col-sm-2"/>
+                    <div className="col-sm-10">
+                        <button className="btn btn-primary btn-block"
+                                onClick={this.login}>
+                            Login
+                        </button>
+                    </div>
+                </div>
+
+                <div className="text-center mt-5">
+                    <h6>Back to homepage? <Link to="/">Cancel</Link></h6>
+                    <h6>Don't have an account? <Link to="/register">Register</Link></h6>
+                </div>
             </div>
         )
     }
--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -36,62 +36,106 @@ export default class RegisterComponent extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Register</h1>
+            <div className='container mt-5'>
+                <h1 className='text-center'>Create your account</h1>
                 {
                     this.state.error &&
                     <div className="alert alert-danger">
                         {this.state.error}
                     </div>
                 }
-                <input
-                    placeholder="username"
-                    value={this.state.user.username}
-                    onChange={(e) => {
-                        const username = e.target.value;
-                        this.setState(prevState => ({
-                            user: {
-                                ...prevState.user,
-                                username: username
+                <div className="form-group row">
+                    <div className="col-sm-2">
+                        <label for="username">
+                            Username
+                        </label>
+                    </div>
+                    <div className="col-sm-10">
+                        <input
+                            className='form-control'
+                            placeholder="username"
+                            id = "username"
+                            value={this.state.user.username}
+                            onChange={(e) => {
+                                const username = e.target.value;
+                                this.setState(prevState => ({
+                                    user: {
+                                        ...prevState.user,
+                                        username: username
+                                    }
+                                }))
                             }
-                        }))
-                    }
-                    }/>
-                <br/>
-                <input
-                    placeholder="password"
-                    type="password"
-                    value={this.state.user.password}
-                    onChange={(e) => {
-                        const password = e.target.value;
-                        this.setState(prevState => ({
-                            user: {
-                                ...prevState.user,
-                                password: password
+                            }/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="col-sm-2">
+                        <label htmlFor="password">
+                            Password
+                        </label>
+                    </div>
+                    <div className="col-sm-10">
+                        <input
+                            className='form-control'
+                            placeholder="password"
+                            id = "password"
+                            type="password"
+                            value={this.state.user.password}
+                            onChange={(e) => {
+                                const password = e.target.value;
+                                this.setState(prevState => ({
+                                    user: {
+                                        ...prevState.user,
+                                        password: password
+                                    }
+                                }))
                             }
-                        }))
-                    }
-                    }/>
-                <br/>
-                <input
-                    placeholder="validate password"
-                    type="password"
-                    value={this.state.user.verifyPassword}
-                    onChange={(e) => {
-                        const verifyPassword = e.target.value;
-                        this.setState(prevState => ({
-                            user: {
-                                ...prevState.user,
-                                verifyPassword: verifyPassword
+                            }/>
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <div className="col-sm-2">
+                        <label for="verifyPassword">
+                            Verify Password
+                        </label>
+                    </div>
+                    <div className="col-sm-10">
+                        <input
+                            className='form-control'
+                            placeholder="validate password"
+                            type="password"
+                            id = "verifyPassword"
+                            value={this.state.user.verifyPassword}
+                            onChange={(e) => {
+                                const verifyPassword = e.target.value;
+                                this.setState(prevState => ({
+                                    user: {
+                                        ...prevState.user,
+                                        verifyPassword: verifyPassword
+                                    }
+                                }))
                             }
-                        }))
-                    }
-                    }/>
-                <br/>
-                <button onClick={this.register}>
-                    Register
-                </button>
-                <Link to="/login">Sign in</Link>
+                            }/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <span className="col-sm-2"/>
+                    <div className="col-sm-10">
+                        <button className="btn btn-primary btn-block"
+                                onClick={this.register}>
+                            Register
+                        </button>
+                        <div className="row">
+                            <div className="col-sm-6 mt-3">
+                                <Link to="/login">Login</Link>
+                            </div>
+                            <div className="col-sm-6 mt-3">
+                                <Link to="/" className="float-right">Cancel</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
